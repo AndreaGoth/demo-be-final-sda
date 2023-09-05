0c6e72c0a1fe821bd95605f21bb80a46f44351f9
Andrei Protopopescu <crazyaffirmative@gmai.com>
2023-09-05 20:31:16 +0300

    created data rest config to enable specific access for http requests

---
+++ src/main/java/com/example/finalprojectexample/config/MyDataRestConfig.java
@@ -0,0 +1,54 @@
+package com.example.finalprojectexample.config;
+
+import com.example.finalprojectexample.entity.Country;
+import com.example.finalprojectexample.entity.Product;
+import com.example.finalprojectexample.entity.ProductCategory;
+import com.example.finalprojectexample.entity.State;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+// first thing that's being configurated in the project
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+    // implements = inherits different methods from -->>
+    // -->> RepositoryRestConfigurer = we are interested in restricted access from certain types of requests
+
+    // EntityManager from Hibernate JPA helps us interogate db
+    private EntityManager entityManager;
+
+    @Autowired
+    // Autowired assures that the connection between the connection
+    public MyDataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        // We override the method due to line 12 where we implement the RepositoryRestConfigurer
+        // Use generate instead of writing (easier & quicker)
+
+        // List containing restricted HTTP requests
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+        // Restricting fundamental operetaions from CRUD which offers a security layer
+
+        // Disable HTTP method for each Repository Resource: Product, ProductCategory, State, Country
+        // Only leaves Get Product, Get ProductCategory, Get State, Get Country working
+        disableHttpMethods(Product.class, config, theUnsupportedActions);
+        disableHttpMethods(ProductCategory.class, config, theUnsupportedActions);
+        disableHttpMethods(State.class, config, theUnsupportedActions);
+        disableHttpMethods(Country.class, config, theUnsupportedActions);
+    }
+
+    // Declaring disabledHttpMethods below:
+    private void disableHttpMethods(Class theClass, RepositoryRestConfiguration configuration, HttpMethod[] theUnsupportedAction) {
+        configuration.getExposureConfiguration()
+                .forDomainType(theClass)
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedAction))
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedAction));
+    }
+}
